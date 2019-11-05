14557f8de671168073b4288f83d2f8073ee8c681
David <david--parrish@outlook.com>
2019-11-05 11:53:11 +0000

    Changed to persistent storage for spotify api tokens

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,36 @@
 import * as Redux from "redux";
-import { combineReducers } from "redux";
-import { spotifyApiReducer } from "./spotify.store/spotify.reducer";
+import { combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
-import { Credentials } from "./spotify.store/spotify.types";
 import SpotifyWebApi from "spotify-web-api-node";
+import {
+  persistStore,
+  persistReducer,
+  PersistConfig,
+  Storage,
+} from "redux-persist";
+import { spotifyApiReducer } from "./spotify.store/spotify.reducer";
+import { Credentials } from "./spotify.store/spotify.types";
+import { AsyncStorage } from "react-native";
 export interface IState {
   spotifyApi: { api: SpotifyWebApi; credentials: Credentials };
 }
+
+const persistConfig: PersistConfig<Storage> = {
+  key: "root",
+  storage: AsyncStorage,
+};
 
 const rootReducer = combineReducers<IState>({
   spotifyApi: spotifyApiReducer,
 });
 
-export const store = Redux.createStore(
-  rootReducer,
-  Redux.applyMiddleware(thunk)
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const Store = () => {
+  const reduxStore = createStore(
+    persistedReducer,
+    Redux.applyMiddleware(thunk)
+  );
+  const persistor = persistStore(reduxStore);
+  return { reduxStore, persistor };
+};
