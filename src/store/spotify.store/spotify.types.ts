777f25b21915bd9b43d8ffb7943ed88b9737dfa7
David <david--parrish@outlook.com>
2019-11-06 22:30:57 +0000

    Updated types

--- src/store/spotify.store/spotify.types.ts
+++ src/store/spotify.store/spotify.types.ts
@@ -1,4 +1,5 @@
 import SpotifyWebApi from "spotify-web-api-node";
+import Spotify from "spotify-web-api-js";
 
 export const SAVE_API = "SAVE_API";
 
@@ -6,5 +7,5 @@ export type StateType_Spotify = SpotifyWebApi;
 
 export type ActionTypes_Spotify = {
   type: typeof SAVE_API;
-  payload: SpotifyWebApi;
+  payload: Spotify.SpotifyWebApiJs;
 };
